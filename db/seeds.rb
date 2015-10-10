6bd20de2ef29957e1440d8321b1cb8665612c7de
Alexashooo <aleksandar.sunjic@gmail.com>
2015-10-10 23:20:42 +0200

    Authorization checkpoint

--- db/seeds.rb
+++ db/seeds.rb
@@ -43,11 +43,20 @@ posts = Post.all
   )
 end
 
-user = User.first
- user.update_attributes!(
-   email: 'aleksandar.sunjic@gmail.com', # replace this with your personal email
-   password: 'helloworld'
- )
+# Create an admin user
+admin = User.create!(
+  name:     'Admin User',
+  email:    'admin@example.com',
+  password: 'helloworld',
+  role:     'admin'
+)
+
+# Create a member
+member = User.create!(
+  name:     'Member User',
+  email:    'member@example.com',
+  password: 'helloworld'
+)
 
 puts "Seed finished"
 puts "#{User.count} users created"
