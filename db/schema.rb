ea60379889a5c7c0ef5be95832c906c51b82ab9c
Alexashooo <aleksandar.sunjic@gmail.com>
2015-10-10 17:07:05 +0200

    Posts and users checkpoint

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151004102054) do
+ActiveRecord::Schema.define(version: 20151009183122) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
@@ -28,9 +28,11 @@ ActiveRecord::Schema.define(version: 20151004102054) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "topic_id"
+    t.integer  "user_id"
   end
 
   add_index "posts", ["topic_id"], name: "index_posts_on_topic_id"
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id"
 
   create_table "questions", force: :cascade do |t|
     t.string   "title"
@@ -44,12 +46,12 @@ ActiveRecord::Schema.define(version: 20151004102054) do
     t.string   "title"
     t.text     "body"
     t.integer  "price"
-    t.integer  "Topic_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "topic_id"
   end
 
-  add_index "sponsored_posts", ["Topic_id"], name: "index_sponsored_posts_on_Topic_id"
+  add_index "sponsored_posts", ["topic_id"], name: "index_sponsored_posts_on_topic_id"
 
   create_table "topics", force: :cascade do |t|
     t.string   "name"
