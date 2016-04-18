1ea267efc994846c01b317cf8156ee6153e6ca47
Alexashooo <aleksandar.sunjic@gmail.com>
2016-04-18 22:30:39 +0200

    Master conflict with Lables_checkout fixed

--- app/models/label.rb
+++ app/models/label.rb
@@ -1,17 +1,15 @@
 class Label < ActiveRecord::Base
 
-<<<<<<< HEAD
+
    has_many :labelings
    belongs_to :labelable, polymorphic: true
    has_many :topics, through: :labelings, source: :labelable, source_type: :Topic
    has_many :posts, through: :labelings, source: :labelable, source_type: :Post
 
-
-=======
   has_many :labelings
   has_many :topics, through: :labelings, source: :labelable, source_type: :Topic
   has_many :posts, through: :labelings, source: :labelable, source_type: :Post
->>>>>>> Labels_checkout
+
 
   def self.update_labels(label_string)
  # #24
