f9a75a0de14f4931599a1ef1e7fccd832a6d3319
Alexashooo <aleksandar.sunjic@gmail.com>
2016-04-19 23:04:04 +0200

    Voting checkpoint, does not work

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,8 @@ Rails.application.routes.draw do
      resources :comments, only: [:create, :destroy]
   end
 
-
+  post '/up-vote' => 'votes#up_vote', as: :up_vote
+  post '/down-vote' => 'votes#down_vote', as: :down_vote
 
   resources :users, only: [:new, :create]
 
