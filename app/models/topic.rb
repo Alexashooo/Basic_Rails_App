3d4dc44fc8810d25722a7d00e669b7ddf6aa5633
Alexashooo <aleksandar.sunjic@gmail.com>
2016-04-24 10:58:56 +0200

    Private topics, not finished

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -5,4 +5,6 @@ class Topic < ActiveRecord::Base
     has_many :labels, through: :labelings
 
     validates :name, length: { minimum: 5 }, presence: true
+
+    scope :visible_to, -> (user) { user ? all : where(public: true) }
 end
