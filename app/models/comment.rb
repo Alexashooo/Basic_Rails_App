35f7aa66347bd00016b395983e5bee4971588e81
Alexashooo <aleksandar.sunjic@gmail.com>
2016-04-24 14:28:58 +0200

    Ajax checkpoint is finished

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,10 +1,11 @@
 class Comment < ActiveRecord::Base
   belongs_to :post
-
   belongs_to :user
 
   validates :body, length: { minimum: 5 }, presence: true
   validates :user, presence: true
+
+  default_scope { order('updated_at DESC') }
 
   after_create :send_favorite_emails
 
@@ -15,5 +16,5 @@ class Comment < ActiveRecord::Base
        FavoriteMailer.new_comment(favorite.user, post, self).deliver_now
      end
    end
-   
+
 end
